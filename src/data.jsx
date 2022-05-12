d27114d61da74354d0f9e0bca8a2424035dd059a
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-05-12 08:46:43 -0700

    mock server is working and test is only passing if I am calling a character from my data.jsx file

---
+++ src/data.jsx
@@ -0,0 +1,63 @@
+export const pokemonApiData = {
+  results: [
+    {
+      _id: '5ff4fb7cd89993a89cc6544f',
+      pokemon: 'butterfree',
+      id: 16,
+      type_1: 'bug',
+      type_2: 'flying',
+      attack: 45,
+      defense: 50,
+      hp: 60,
+      ability_1: 'compound-eyes',
+      egg_group_1: 'bug',
+      egg_group_2: 'NA',
+      url_image:
+        'http://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png',
+      shape: 'bug-wings',
+      pokebase: 'butterfree',
+      pokedex: 'http://www.pokemon.com/us/pokedex/butterfree',
+    },
+    {
+      _id: '5ff4fb7cd89993a89cc65444',
+      pokemon: 'venusaur-mega',
+      id: 4,
+      type_1: 'grass',
+      type_2: 'poison',
+      attack: 100,
+      defense: 123,
+      hp: 80,
+      ability_1: 'thick-fat',
+      egg_group_1: 'monster',
+      egg_group_2: 'plant',
+      url_image:
+        'http://assets.pokemon.com/assets/cms2/img/pokedex/full/003_f2.png',
+      shape: 'NA',
+      pokebase: 'venusaur',
+      pokedex: 'http://www.pokemon.com/us/pokedex/venusaur',
+    },
+    {
+      _id: '5ff4fb7cd89993a89cc6545e',
+      pokemon: 'pikachu',
+      id: 31,
+
+      type_1: 'electric',
+      type_2: 'NA',
+      attack: 55,
+      defense: 40,
+      hp: 35,
+      special_attack: 50,
+      special_defense: 50,
+      speed: 90,
+      ability_1: 'static',
+
+      egg_group_1: 'ground',
+      egg_group_2: 'fairy',
+      url_image:
+        'http://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
+
+      pokebase: 'pikachu',
+      pokedex: 'http://www.pokemon.com/us/pokedex/pikachu',
+    },
+  ],
+};
